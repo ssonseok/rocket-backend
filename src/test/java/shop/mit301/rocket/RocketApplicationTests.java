a5ee26e3d2160351ca644014208ec897bca1d290
Username33370 <wiggles@naver.com>
2025-09-12 10:06:52 +0900

    Initial commit

---
+++ src/test/java/shop/mit301/rocket/RocketApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mit301.rocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
