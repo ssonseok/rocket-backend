43769d5090a07187e3c10173b0aeb92d0eaee99f
suckerson <tjrgus1849@naver.com>
2025-11-03 13:23:53 +0900

    gitignore 처리

--- src/main/java/shop/mit301/rocket/config/MailConfig.java
+++ src/main/java/shop/mit301/rocket/config/MailConfig.java
@@ -1,5 +1,6 @@
 package shop.mit301.rocket.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,14 +10,19 @@ import java.util.Properties;
 
 @Configuration
 public class MailConfig {
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
 
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("mit33370@gmail.com");
-        mailSender.setPassword("hmkv qyoc blap syps");
+        mailSender.setUsername(this.username);
+        mailSender.setPassword(this.password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
